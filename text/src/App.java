add0333eef3e5c601360fc4a4bf845a13cd6ca4d
Blacktuna1 <1037859047@qq.com>
2022-08-06 15:03:46 +0800

    8.7
    8.7

---
+++ text/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int i=0;i<=100;i++){
+            System.out.print("<br>");
+        }
+        
+    }
+}
